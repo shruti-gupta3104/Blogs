fab899516ddefc90dbaf789df44476b6ea04e7f7
shruti gupta3104 <shrutigupta3104@gmail.com>
2025-07-17 01:23:03 +0530

    second commit

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -7,7 +7,7 @@ import BlogDetails from './BlogDetails';
 const Blogs = () => {
     const{loading,posts} = useContext(AppContext)
   return (
-    <div className='w-11/12 h-screen max-w-[670px] py-8 flex flex-col  mt-[285px] mb-[35%] justify-center items-center '>
+    <div className='w-11/12 h-screen max-w-[670px] py-8 flex flex-col  mt-[285px] mb-[42%] justify-center items-center '>
       {
         loading ? (<Spinner/>): (
             posts.length === 0 ? (<div>
